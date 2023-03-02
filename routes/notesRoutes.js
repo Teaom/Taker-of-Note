1bcea075e90ac6363224e9191575820ef996fb3f
Teaom <dclarkjenkins87@gmail.com>
2023-03-02 11:48:19 -0700

    Notes Route

--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -6,3 +6,26 @@ const {
 const {
     readAndAppend, readFromFile, deleteNote
 } = require('../helpers/fs');
+
+Router.get('/api/notes', (req, res) => {
+    const { title, text } =req.body;
+
+    if (title && text) {
+        const newNote ={
+            id: uuidv4(),
+            title,
+            text
+        };
+        readAndAppend( newNote, './db/db.json');
+
+        const interfaceResponse = {
+            status: 'New Notes Added',
+            body: newNote
+        }
+        res.json(interfaceResponse);
+        } else {
+            res.json('Error adding notes');
+    }
+});
+
+module.exports = router;
